8a7a3e9880c114a8cb8589cafb1ca62225ed2541
Alessandro Manfredi <alle.manfredi@gmail.com>
2024-04-04 08:12:16 +0200

    feat(global): updates solidity version

--- contracts/interfaces/hashi/IGiriGiriBashi.sol
+++ contracts/interfaces/hashi/IGiriGiriBashi.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-only
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.23;
 
 interface IGiriGiriBashi {
     function getThresholdHash(uint256 domain, uint256 id) external view returns (bytes32);
